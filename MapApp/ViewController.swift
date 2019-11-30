811df6624433033685b1d098158c7a0782fd0867
Ryo <ryoouchi0315@gmail.com>
2019-11-30 18:54:48 +0900

    Map is not working

--- MapApp/ViewController.swift
+++ MapApp/ViewController.swift
@@ -14,17 +14,37 @@ import PopupDialog
 
 class ViewController: UIViewController {
     
-    
+    @IBOutlet weak var mapView: GMSMapView!
     @IBOutlet weak var gameScoreView: UIView!
     @IBOutlet weak var diamondScoreView: UIView!
     @IBOutlet weak var butterflyScoreView: UIView!
     @IBOutlet weak var featherScoreView: UIView!
+    @IBOutlet weak var diamondScoreLabel: UILabel!
+    @IBOutlet weak var butterflyScoreLabel: UILabel!
+    @IBOutlet weak var featherScoreLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpScoreView()
     }
+    
 
+
+    //MARK: ScoreView
+    func setUpScoreView() {
+            self.view.bringSubviewToFront(gameScoreView)
+            gameScoreView.layer.cornerRadius = 10
+            gameScoreView.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMaxXMaxYCorner]
+            
+            diamondScoreView.layer.cornerRadius = 25
+            butterflyScoreView.layer.cornerRadius = 25
+            featherScoreView.layer.cornerRadius = 25
+            
+            diamondScoreView.layer.masksToBounds = true
+            butterflyScoreView.layer.masksToBounds = true
+            featherScoreView.layer.masksToBounds = true
+        }
+    
 
 }
 
